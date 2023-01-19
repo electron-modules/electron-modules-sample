676ec7dfad9c421834e2a53e18ddc416e8a67f95
xudafeng <xudafeng@126.com>
2023-01-19 17:27:26 +0800

    feat: init electrom

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,2 +1,8 @@
 'use strict';
 
+document.querySelectorAll('[data-action]').forEach(elem => {
+  elem.addEventListener('click', () => {
+    const action = elem.getAttribute('data-action');
+    window._electron_bridge.send('start-action', action);
+  });
+});
