005c3310b207e51d6f84dcd65223488cd3b4c0d0
xudafeng <xudafeng@126.com>
2023-01-20 16:18:24 +0800

    feat: log version

--- src/updator/index.ts
+++ src/updator/index.ts
@@ -2,6 +2,9 @@
 
 import crypto from 'crypto';
 import ElectronUpdator from 'electron-updator';
+import { version as ElectronUpdatorVersion } from 'electron-updator/package';
+
+console.log('version: %s', ElectronUpdatorVersion);
 
 const { MacUpdator, EventType } = ElectronUpdator;
 
@@ -22,19 +25,25 @@ module.exports = (app: any) => {
   const electronUpdator = new MacUpdator(options);
   // 3. 绑定全局事件
   electronUpdator.on(EventType.UpdateDownloaded, (needRemind) => {
+    console.log(EventType.UpdateDownloaded);
     console.log(needRemind);
   });
   electronUpdator.on(EventType.CheckingForUpdate, (needRemind) => {
+    console.log(EventType.CheckingForUpdate);
     console.log(needRemind);
   });
-  app.electronUpdator.on(EventType.Error, (needRemind) => {
+  electronUpdator.on(EventType.Error, (needRemind) => {
+    console.log(EventType.Error);
     console.log(needRemind);
   });
   electronUpdator.on(EventType.UpdateDownloadProgress, (data) => {
+    console.log(EventType.UpdateDownloadProgress);
   });
   electronUpdator.on(EventType.UpdateNotAvailable, () => {
+    console.log(EventType.UpdateNotAvailable);
   });
   electronUpdator.on(EventType.UpdateClose, () => {
+    console.log(EventType.UpdateClose);
   });
   app.electronUpdator = electronUpdator;
 };
