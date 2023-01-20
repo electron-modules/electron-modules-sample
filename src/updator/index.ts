d583a08d090791d912016e8adc90560d6cb5b183
xudafeng <xudafeng@126.com>
2023-01-20 14:24:30 +0800

    feat: add updator instance

--- src/updator/index.ts
+++ src/updator/index.ts
@@ -1,7 +1,40 @@
 'use strict';
 
+import crypto from 'crypto';
 import ElectronUpdator from 'electron-updator';
 
+const { MacUpdator, EventType } = ElectronUpdator;
+
 module.exports = (app: any) => {
-  // console.log(ElectronUpdator);
+  // 1. 构造 options
+  const verify = crypto.createVerify('SHA256');
+  const options = {
+    logger: console, // logger
+    verify, // verify
+    verifyPublicKey: '',
+    requestFeedUrl: '',
+    productName: '-',
+    responseFormatter: (res) => {
+      console.log(res);
+    },
+  };
+  // 2. 初始化 updator 实例
+  const electronUpdator = new MacUpdator(options);
+  // 3. 绑定全局事件
+  electronUpdator.on(EventType.UpdateDownloaded, (needRemind) => {
+    console.log(needRemind);
+  });
+  electronUpdator.on(EventType.CheckingForUpdate, (needRemind) => {
+    console.log(needRemind);
+  });
+  app.electronUpdator.on(EventType.Error, (needRemind) => {
+    console.log(needRemind);
+  });
+  electronUpdator.on(EventType.UpdateDownloadProgress, (data) => {
+  });
+  electronUpdator.on(EventType.UpdateNotAvailable, () => {
+  });
+  electronUpdator.on(EventType.UpdateClose, () => {
+  });
+  app.electronUpdator = electronUpdator;
 };
