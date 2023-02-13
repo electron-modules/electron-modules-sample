0baa776aa5d76bca8a9c4853b00ed73c976d534e
xudafeng <xudafeng@126.com>
2023-02-13 19:52:58 +0800

    feat: tweak demo

--- src/windows-verify-trust/index.ts
+++ src/windows-verify-trust/index.ts
@@ -5,7 +5,7 @@ import { isLibExist, getLibPath, verifyTrust } from 'windows-verify-trust';
 
 module.exports = (app: any) => {
   const targetFileName = 'wlanapi.dll';
-  console.log('isLibExist: %b', isLibExist(targetFileName));
+  console.log('isLibExist: %s', isLibExist(targetFileName));
   console.log('getLibPath: %s', getLibPath(targetFileName));
   console.log('verifyTrust: %s', verifyTrust(targetFileName));
 };
