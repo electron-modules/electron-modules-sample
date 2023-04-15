d98c91f2fba2341e7816a9df3d3de64df64c1813
xudafeng <xudafeng@126.com>
2023-04-16 00:26:55 +0800

    feat: add indexeddb

--- src/local-storage/renderer/main.js
+++ src/local-storage/renderer/main.js
@@ -3,6 +3,19 @@
 class MainApp {
   constructor() {
     this.perfboardElemContainer = document.querySelector('#perf-board');
+    this.SQLiteLogsContainer = document.querySelector('#SQLiteLogs');
+    this.IndexedDBLogsContainer = document.querySelector('#IndexedDBLogs');
+    this.runOptions = {
+      IndexedDB: 'Default',
+      SQLite: 'ElectronNative',
+    };
+    this.logs = {
+      IndexedDB: [],
+      SQLite: [],
+    };
+    this.runnerConfig = {
+      count: 10 * 10000,
+    };
     this.init();
   }
 
@@ -44,8 +57,77 @@ class MainApp {
           e.preventDefault();
           window._electron_bridge.ipcRenderer.send('openExternal', target.href);
         }
+      } else if (target.nodeName === 'BUTTON') {
+        const type = e.target.getAttribute('data-type');
+        this.run(type);
       }
     }, false);
+    document.querySelectorAll('form')
+      .forEach(form => {
+        form.addEventListener('change', (e) => {
+          this.runOptions[e.target.name] = e.target.value;
+        }, false);
+      });
+  }
+
+  run(type) {
+    const method = `run${type}${this.runOptions[type]}`;
+    this.log(type, `type: ${type} options: ${this.runOptions[type]}`);
+    this.log(type, `start run: ${method}`);
+    this[method]?.(type);
+  }
+
+  async runIndexedDBDefault(type) {
+    await window.indexedDBHelper.deleteAllIndexedDB();
+    const storeName = 'myData1';
+    const objectStore = await window.indexedDBHelper.getObjectStore('myDatabase1', 10, storeName);
+    const { count } = this.runnerConfig;
+    const startTime = Date.now();
+    this.log(type, `start time: ${startTime}, count: ${count}`);
+    await window.indexedDBHelper.addBatchTestData(objectStore, count);
+    const endTime = Date.now();
+    this.log(type, `end time: ${endTime}, use ${((endTime - startTime) / 1000).toFixed(2)}s`);
+  }
+
+  async runIndexedDBDexie(type) {
+    await window.indexedDBHelper.deleteAllIndexedDB();
+    const storeName = 'myData1';
+    const db = new window.Dexie('myDatabase2');
+    db.version(1).stores({
+      [storeName]: 'index1, field1',
+    });
+    const { count } = this.runnerConfig;
+    const startTime = Date.now();
+    this.log(type, `start time: ${startTime}, count: ${count}`);
+    for (let i = 0; i < count; i++) {
+      await db[storeName].add({
+        index1: `index_${i}`,
+        field1: new Array(100).fill('测试').join(''),
+      });
+      console.log('Data added successfully');
+    }
+    const endTime = Date.now();
+    this.log(type, `end time: ${endTime}, use ${((endTime - startTime) / 1000).toFixed(2)}s`);
+  }
+
+  async runSQLiteElectronNative(type) {
+    const { count } = this.runnerConfig;
+    const startTime = Date.now();
+    this.log(type, `start time: ${startTime}, count: ${count}`);
+    for (let i = 0; i < count; i++) {
+      await window._electron_bridge.addTestData({
+        index1: `index_${i}`,
+        field1: new Array(100).fill('测试').join(''),
+      });
+      console.log('Data added successfully');
+    }
+    const endTime = Date.now();
+    this.log(type, `end time: ${endTime}, use ${((endTime - startTime) / 1000).toFixed(2)}s`);
+  }
+
+  log(type, content = '') {
+    this.logs[type].unshift(content);
+    this[`${type}LogsContainer`].innerHTML = this.logs[type].join('\n');
   }
 }
 
--- src/local-storage/renderer/preload.js
+++ src/local-storage/renderer/preload.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const { contextBridge, ipcRenderer } = require('electron');
+const sqlite3 = require('@journeyapps/sqlcipher').verbose();
+const db = new sqlite3.Database('test.db');
 
 contextBridge.exposeInMainWorld('_electron_bridge', {
   ipcRenderer: {
     send: (channel, ...args) => ipcRenderer.send(channel, ...args),
+  },
+  addTestData: async () => {
+    const res = await db.all('');
+    return res;
   },
 });
