b98a652e26a763c36fceeb8994acf2465db53e48
xudafeng <xudafeng@126.com>
2023-09-10 15:13:45 +0800

    chore: update build

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -5,14 +5,31 @@ module.exports = async function () {
     appId: 'electron.modules.sample',
     copyright: 'ElectronModules',
     productName: 'ElectronModules',
+    forceCodeSigning: true,
     asar: true,
     npmRebuild: false,
     directories: {
       output: 'dist/'
     },
+    linux: {
+      target: 'deb',
+    },
     dmg: {
-      'writeUpdateInfo': true
-    }
+      writeUpdateInfo: false,
+    },
+    mac: {
+      target: [
+        {
+          target: 'dmg',
+          arch: ['x64', 'arm64'],
+        },
+      ],
+      publish: [
+        {
+          provider: 'github',
+        },
+      ],
+    },
   };  
   return config;
 };
